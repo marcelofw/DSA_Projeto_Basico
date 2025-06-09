02edea63892704f808075b533bfd5223b12a0c06
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2025-06-09 10:59:20 -0300

    updating

---
+++ project2.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime as dt
+
+df_dsa = pd.read_csv("dataset.csv")
+# print(df_dsa.shape)
+# print(type(df_dsa))
+# print(df_dsa.dtypes)
+# df_dsa.info()
+# print(df_dsa.head(10))
+# print(df_dsa.tail())
+# print(df_dsa.columns)
+# print(list(df_dsa.columns))
+# print(df_dsa["Valor_Venda"].describe())
+# print(df_dsa["Valor_Venda"])
+# print(df_dsa.Valor_Venda)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
