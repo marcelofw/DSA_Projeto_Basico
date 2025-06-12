34bbd0d7396110f88ddf7e0c521f68c3224591ef
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2025-06-12 12:46:48 -0300

    uploading

--- project2.py
+++ project2.py
@@ -16,19 +16,18 @@ df_dsa = pd.read_csv("dataset.csv")
 # print(df_dsa["Valor_Venda"].describe())
 # print(df_dsa["Valor_Venda"])
 # print(df_dsa.Valor_Venda)
-
 # print(df_dsa[df_dsa.duplicated()])
 # print(df_dsa[df_dsa["Valor_Venda"] > 200.0])
+# print(df_dsa.isnull().sum())
 
-
-
-
-
-
-
-
-
-
+df_dsa_p1 = df_dsa[df_dsa["Categoria"] == "Office Supplies"]
+# print(df_dsa_p1)
+# print(df_dsa["Categoria"] == "Office Supplies")
+df_dsa_p1_total = df_dsa_p1.groupby("Cidade")["Valor_Venda"].sum()
+# print(df_dsa_p1_total)
+# print(type(df_dsa_p1_total))
+# print(df_dsa_p1.groupby("Cidade")[["Valor_Venda"]].sum())
+# print(type(df_dsa_p1.groupby("Cidade")[["Valor_Venda"]].sum()))
 
 
 
